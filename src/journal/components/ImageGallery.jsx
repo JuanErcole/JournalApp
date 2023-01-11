193e14a61153bf1e28d1343ba8241b259bd7b147
Juan Ercole <juan.m.ercole@gmail.com>
2023-01-11 19:23:35 -0300

    arreglando imageGallery

--- src/journal/components/ImageGallery.jsx
+++ src/journal/components/ImageGallery.jsx
@@ -7,7 +7,7 @@ export const ImageGallery= ({ images }) => {
 
   return (
     <ImageList sx={{ width: '100%', height: 500 }} cols={3} rowHeight={200}>
-      {images.map((image) => (
+      {images?.map((image) => (
         <ImageListItem key={image}>
           <img
             style={{ height:'100%' }}
